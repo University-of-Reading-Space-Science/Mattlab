4bc9a28fccfe15ecf9e70b27e636777132f0a573
Mathew Owens <31659031+mathewjowens@users.noreply.github.com>
2024-03-20 14:40:38 +0000

    Update mplot.py
    moved the speed map plot to mplot. deleted huxt_plots

--- code/mplot.py
+++ code/mplot.py
@@ -38,6 +38,7 @@ def getconfidintervals(endata,confid_intervals):
             confid_ts[t,2*nconfid+2] = np.percentile(dist,100-confid_intervals[nconfid])
     return confid_ts
 
+
 #plot the percentiles
 def plotconfidbands(tdata, endata, confid_intervals = [5,10,32], plot_legend=False):
     
@@ -65,8 +66,11 @@ def plotconfidbands(tdata, endata, confid_intervals = [5,10,32], plot_legend=Fal
     if plot_legend:
         plt.legend(facecolor='silver')    
         
+        
 def odr_lin_regress(xvals, yvals, xerr = None, yerr = None, p0 = [1., 2.]):
     """
+    Orthogonal distance regression
+    
     Parameters
     ----------
     x : xdata
@@ -101,6 +105,7 @@ def odr_lin_regress(xvals, yvals, xerr = None, yerr = None, p0 = [1., 2.]):
     myodr = odr.ODR(mydata, linear, beta0=p0)
     output = myodr.run()
     return output.beta
+
 
 def ols_odr_example(n = 50, xsigma = 2., ysigma = 1.) : 
     """
@@ -397,3 +402,40 @@ def mengZ(r1,n1, r2, n2, printoutput = True):
         
     return p_value
 
+def plotspeedmap(vr_map, vr_longs, vr_lats, fig = None, ax = None):
+    # a function to plot a speed map, such as output from WSA or MAS.
+    
+    
+    if fig == None:
+        fig = plt.figure(figsize = (9,4.5))
+    
+    if ax == None:
+        ax = plt.subplot(1,1,1)
+    
+    pc = ax.pcolor(vr_longs.value*180/np.pi, vr_lats.value*180/np.pi, vr_map.value, 
+               shading='auto',vmin=250, vmax=650)
+
+    ax.set_ylim([-90,90]); ax.set_xlim([0,360])
+    ax.set_xlabel('Carrington Longitude [deg]')
+    ax.set_ylabel('Latitude [deg]')
+
+
+    ax.axes.xaxis.set_ticks([0,90,180,270,360])
+    ax.axes.yaxis.set_ticks([-90,-45,0,45,90])
+    #ax.axes.xaxis.set_ticklabels([])
+    #ax.axes.yaxis.set_ticklabels([])
+    plt.sca(ax)
+    #colorbar
+    axins = inset_axes(ax,
+                        width="100%",  # width = 50% of parent_bbox width
+                        height="10%",  # height : 5%
+                        loc='upper right',
+                        bbox_to_anchor=(0.28, 0.58, 0.72, 0.5),
+                        bbox_transform=ax.transAxes,
+                        borderpad=0,)
+    cb = fig.colorbar(pc, cax = axins, orientation = 'horizontal',  pad = -0.1)
+    cb.ax.xaxis.set_ticks_position("top")
+    ax.text(0.15,1.05,r'$V_{SW}$ [km/s]' , 
+            fontsize = 11, transform=ax.transAxes, backgroundcolor = 'w')
+    
+    return fig, ax, axins
